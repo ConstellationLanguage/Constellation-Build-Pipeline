a537c0ead640de0538e9fecea34e76e087704f39
acharton <antoine.charton@gmail.com>
2018-05-26 17:55:32 -0400

    Added build file

--- UpdateProject.bat
+++ UpdateProject.bat
@@ -1,5 +1,41 @@
-xcopy /s %~dp0\Repositories\Constellation-Flappy-bird\FlappyBird\Assets\FlappyBirdExample %~dp0\BuildProject\Constellation\Assets\FlappyBirdExample
+del /s /q %~dp0BuildProject\Constellation
 
-xcopy /s %~dp0\Repositories\ConstellationMaster\Constellation\Assets\Constellation %~dp0\BuildProject\Constellation\Assets\Constellation
+set rootDisplay = %~dp0
 
-xcopy /s %~dp0\Repositories\ConstellationMaster\Constellation\Assets\Gizmos %~dp0\BuildProject\Constellation\Assets\Constellation
+cd %~dp0Repositories\ConstellationFlappyBird
+set HOME=%USERPROFILE%
+git pull origin master
+
+cd rootDisplay
+
+cd %~dp0Repositories\ConstellationMaster
+set HOME=%USERPROFILE%
+git pull origin master
+
+
+cd rootDisplay
+
+setlocal
+
+rem Define base folder to Processing
+set TopLevel= %~dp0BuildProject
+
+rem process each folder contained in base folder
+for /d %%t in ("%TopLevel%\*.*") do (
+    rem Display folder currently being processed
+	echo Processing '%%~t' ...
+
+    rem remove files older than one day from this folder
+    rem *** add /s option to pure old files in all subfolders as well ***
+    forfiles /p "%%~t" /d -1 /c "cmd /c del /s @path"
+
+    rem Remove any empty folders below thus user folder
+	for /f "delims=" %%i in ('dir /s /b /ad "%%t" ^| sort /r') do (rd "%%~i" >NUL
+    )
+)
+
+xcopy /s %~dp0Repositories\ConstellationFlappyBird\FlappyBird\Assets\FlappyBirdExample %~dp0BuildProject\Constellation\Assets\FlappyBirdExample
+
+xcopy /s %~dp0Repositories\ConstellationMaster\Constellation\Assets\Constellation %~dp0BuildProject\Constellation\Assets\Constellation
+
+xcopy /s %~dp0Repositories\ConstellationMaster\Constellation\Assets\Gizmos %~dp0BuildProject\Constellation\Assets\Constellation
